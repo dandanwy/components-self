4a60982a2a4501989d0d08ab04539443c9ad1353
LAPTOP-NJTJL3I7\wy <wangyan@gzrqian.com>
2018-08-20 16:56:21 +0800

    封装查询条件组件，实现查询与重置效果

--- src/components/mPanel/mQuery/main.vue
+++ src/components/mPanel/mQuery/main.vue
@@ -2,10 +2,10 @@
     <div id="mQuery">
         <el-row>
             <el-col :span="config.showCollapse ? 22 : 24">
-                <el-form :inline="config.inlineType" :model="config" :style="{maxHeight: collapse[collapseType].maxHeight, overflow: collapse[collapseType].overflow}" class="demo-form-inline">
+                <el-form ref="formData" :inline="config.inlineType" :model="config" :style="{maxHeight: collapse[collapseType].maxHeight, overflow: collapse[collapseType].overflow}" class="demo-form-inline">
                     <el-row v-for="(rowItem, rowIndex) in config.queryData.rows" :key="'row_'+rowIndex">
                         <el-col v-for="(item, index) in config.queryData.items" v-if="item.layout.row===rowItem" :key="'col_' + index" :span="item.layout.col">
-                            <el-form-item :label="item.labelName">
+                            <el-form-item :label="item.labelName" :prop="'queryData.items.' + index + '.labelValue'">
                                 <el-input v-if="item.type === 'input'" v-model="item.labelValue"/>
                                 <el-select v-else-if="item.type === 'select'" v-model="item.labelValue">
                                     <el-option v-for="option in item.options" :value="option.value" :label="option.label" :key="option.key"></el-option>
@@ -25,7 +25,7 @@
                     <el-row type="flex" :justify="(config.buttons&&config.buttons.justify) || 'center'">
                         <el-col :span="6">
                             <el-button type="primary" @click="handleSubmit">{{(config.buttons&&config.buttons.submitText) || '查 询'}}</el-button>
-                            <el-button type="ghost">{{(config.buttons&&config.buttons.resetText) || '重 置'}}</el-button>
+                            <el-button type="ghost" @click="handleReset">{{(config.buttons&&config.buttons.resetText) || '重 置'}}</el-button>
                         </el-col>
                     </el-row>
                 </el-form>
@@ -45,6 +45,7 @@
 </template>
 
 <script>
+    import {parseTime} from '../../../utils/index.js';
     export default {
         name:'mQuery',
         props: {
@@ -86,12 +87,32 @@
             },
             // 查询条件值
             getCondition(config) {
+                let arr = '{';
                 this.config.queryData.items.map((item, index) => {
-                    console.log(item);
+
+                    // if (item.labelValue && (item.labelValue !== '' || item.labelValue.length !== 0)) {
+                        if (item.type === 'DatePicker' || item.type === 'TimePicker') {
+                        if (item.pickerType === 'daterange' || item.pickerType === 'datetimerange') {
+                            item.labelValue[0] = item.labelValue[0] ? parseTime(item.labelValue[0], item.format) : '';
+                            item.labelValue[1] = item.labelValue[1] ? parseTime(item.labelValue[1], item.format) : '';
+                        } else {
+                            item.labelValue = item.labelValue ? parseTime(item.labelValue, item.format) : '';
+                        }
+                        }
+                        arr += '"' + item.labelKey + '":"' + item.labelValue + '",';
+                    // }
                 });
+                if (arr === '{') return {};
+                arr = arr.substring(0, arr.lastIndexOf(',')) + '}';
+                console.log(arr);
+                return JSON.parse(arr);
             },
             handleSubmit() {
                 this.getCondition(this.config);
+            },
+            handleReset() {
+                console.log(this.$refs['formData']);
+                this.$refs['formData'].resetFields();
             }
         },
         mounted () {
